87a4743c6496758eebc27d95e73f700484e4178e
joaogabrielcastro <joaowotekoski@gmail.com>
2025-07-28 15:21:36 -0300

    Finalizando 22

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import ConsultaPorPlaca from "./Pages/ConsultaPorPlaca";
+import ConsultaPorPlaca from "./Pages/ConsultaPelaPlaca";
 import SituacaoAtual from "./Pages/SituacaoAtual";
 import ResumoFinanceiro from "./Pages/ResumoFinanceiro";
 import EditarViagem from "./Pages/EditarViagem";
 import GraficosDesempenho from "./Pages/GraficosDesempenho";
 import ExportarDados from "./Pages/ExportarDados";
 import CadastroGeral from "./Pages/CadastroGeral";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
   const [showPaymentReminder, setShowPaymentReminder] = useState(false); // NOVO ESTADO
 
   useEffect(() => {
     const today = dayjs();
-    if (today.date() === 15) { 
+    if (today.date() === 15) {
       setShowPaymentReminder(true);
     } else {
       setShowPaymentReminder(false);
@@ -33,11 +33,11 @@ function App() {
 
       <main className="py-8 px-4 sm:px-6 lg:px-8">
         <Routes>
-          <Route path="/" element={<ConsultaPorPlaca />} />
+          <Route path="/" element={<ConsultaPelaPlaca />} />
           <Route path="/situacao" element={<SituacaoAtual />} />
           <Route path="/cadastrar" element={<CadastroGeral />} />
           <Route path="/resumo" element={<ResumoFinanceiro />} />
-          <Route path="/editar/:id?" element={<EditarViagem />} /> 
+          <Route path="/editar/:id?" element={<EditarViagem />} />
           <Route path="/graficos" element={<GraficosDesempenho />} />
           <Route path="/exportar" element={<ExportarDados />} />
         </Routes>
